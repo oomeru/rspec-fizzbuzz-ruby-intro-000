cc3273121faa140a84f57d3d1ab1ecd52eeb285c
oomeru <obaroomeru@gmail.com>
2017-08-25 19:40:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(i)
-  if i % 3 == 0
-    "Fizz"
+  if i % 3 == 0 && i % 5 == 0
+    "FizzBuzz"
   elsif i % 5 == 0
     "Buzz"
-  elsif i % 15 == 0
-    "Buzz"
+  elsif i % 3 == 0
+    "Fizz"
   else i % 3 != 0 || i % 5 != 0
     puts nil
   end
